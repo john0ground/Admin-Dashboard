8f6a380c47f7a7c2eb34a2fa52cf8ef5ab5db744
John <mjohncorbet@gmail.com>
2023-02-07 02:25:29 +0800

    set header to position: sticky

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const date = document.getElementById('currentDate');
 date.textContent = new Date().toDateString();
+
+window.onscroll = function() {
+    const headerUser = document.querySelector('.user-name');
+    const header = document.querySelector('header');
+    if (window.pageYOffset > headerUser.offsetTop) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+}
